8d574b4e4e02dc2300e76d7de705924da8f05f4d
kingofthyhill <s3n4t0rl3m0nsn0ut@gmail.com>
2014-12-07 15:55:41 -0900

    Update 3:55 P.M. 12/7/14 Made some changes to tankThingy and coinDrop.

--- Assets/tankMovement.cs
+++ Assets/tankMovement.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class tankMovement : MonoBehaviour {
 
+	public float speedFactor;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,8 +12,26 @@ public class tankMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if ( false){
-			Debug.Log ("Yay me!");
+		if (Input.GetKey (KeyCode.RightArrow)){
+			rigidbody.velocity += Vector3.right * speedFactor * Mathf.Abs (rigidbody.velocity.x);
+			if (Mathf.Abs (rigidbody.velocity.x) <= 1) {
+				rigidbody.velocity += Vector3.right * 1;
+			}
+		} if (Input.GetKey (KeyCode.LeftArrow)) {
+			rigidbody.velocity += Vector3.left * speedFactor * Mathf.Abs (rigidbody.velocity.x);
+			if (Mathf.Abs (rigidbody.velocity.x) <= 1) {
+				rigidbody.velocity += Vector3.left * 1;
+			}
+		} if (Input.GetKey (KeyCode.UpArrow)) {
+			rigidbody.velocity += Vector3.forward * speedFactor * Mathf.Abs (rigidbody.velocity.z);
+			if (Mathf.Abs (rigidbody.velocity.z) <= 1) {
+				rigidbody.velocity += Vector3.forward * 1;
+			}
+		} if (Input.GetKey (KeyCode.DownArrow)) {
+			rigidbody.velocity += Vector3.back * speedFactor * Mathf.Abs (rigidbody.velocity.z);
+			if (Mathf.Abs (rigidbody.velocity.z) <= 1) {
+				rigidbody.velocity += Vector3.back * 1;
+			}
 		}
 	}
 }
